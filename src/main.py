6718e6593156edeb9dc1eb896d0eb6da5ca74b59
llf <bingo.llf.00@gmail.com>
2025-11-06 15:24:29 +0800

    tmp commit file

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import time
+import torch
 import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
 
 
 class MyNet(nn.Module):
     def __init__(self):
         super(MyNet, self).__init__()
 
+        # (64,1,28,28)
         self.net = nn.Sequential(
+            # (64, 32, 28, 28)
             nn.Conv2d(1, 32, 3),
+            # (64, 64, 28, 28)
             nn.Conv2d(32, 64, 3),
+            # (64, 128, 28, 28)
             nn.Conv2d(64, 128, 3),
+            # (64, 256, 28, 28)
             nn.Conv2d(128, 256, 3),
+            # (64, 512, 28, 28)
             nn.Conv2d(256, 512, 3),
+            # ()
             nn.Linear(512 * 7 * 7, 512),
             nn.Linear(512, 10),
         )
@@ -22,3 +32,21 @@ class MyNet(nn.Module):
 
 if __name__ == '__main__':
     model = MyNet()
+
+    train_dataset = torchvision.datasets.MNIST(
+        "../data/train/", train=True, download=False,
+        transform=torchvision.transforms.ToTensor())
+    test_dataset = torchvision.datasets.MNIST(
+        '../data/test/', train=False, download=False,
+        transform=torchvision.transforms.ToTensor())
+    print(train_dataset)
+    print(test_dataset)
+
+    # (img[batch_size, 1, 28, 28], label[batch_size, 1])
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+    # 测试gpu能否使用
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    print(model)
